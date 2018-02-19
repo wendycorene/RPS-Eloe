c3052748b4af7676314f1dfbc0d92e7d6eb7ce64
Eloe,Wendy C <s524779@ch140029m.nwmissouri.edu>
2018-02-19 17:32:19 -0600

    Changed some names for tabs

---
+++ RPS-Eloe/ResultsView.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsView.swift
+//  RPS-Eloe
+//
+//  Created by Eloe,Wendy C on 2/19/18.
+//  Copyright © 2018 Eloe,Wendy C. All rights reserved.
+//
+
+import UIKit
+
+class ResultsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
